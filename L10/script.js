49293a5ee2aceb51274ab79feb9fc92dbd267138
mashru020 <mashrurrahman16@gmail.com>
2021-07-14 22:27:06 +0600

    132, 133 - bind method , coding challange #1

--- L10/script.js
+++ L10/script.js
@@ -106,7 +106,7 @@ const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 greetArr('Hi')('Mashru');*/
 
-// The call and apply Methods
+/*// 131.The call and apply Methods
 
 const lufthansa = {
     airline: 'Lufthansa',
@@ -162,7 +162,116 @@ console.log(swiss);
 
 // better way to doing apply method in modern js
 book.call(swiss, ...fligntData);
-console.log(swiss);
+console.log(swiss); 
 
 
-// 
+// 132. The Bind Method (with 131)
+
+const bookEW = book.bind(eurowings);
+
+
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Mashrur Rahman');
+bookEW23('Martha Cooper');
+
+// with Event Listener
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function() {
+    console.log(this);
+    this.planes++;
+    console.log(this.planes);
+}
+
+// in event handeler this keyword always point to its attached element. 
+// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
+
+
+// solution is the bind method
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+
+// Partial application
+
+const addText = (rate, value) => value + value * rate ;
+console.log(addText(0.1, 200));
+
+const addVAT = addText.bind(null,0.23);
+//const addVAT = addText.bind(0.23);
+// addVAT = value => value + value * 0.23;
+
+
+// with function return function
+const addText2 = function(rate) {
+    return function(value){
+        return value + value * rate;
+    }
+}
+const addVat2 = addText2(.23);
+console.log(addVat2(200));*/
+
+// 133. Coding Challange #1
+//1. Create a method called 'registerNewAnswer' on the 'pool' object. The method dose 2 things:
+    // 1.1. Display a prompt window for the user to input the number of the selection option. The prompt should look like this: 
+    //     What is your favourite Programming Language?
+    //     0: JavaScript
+    //     1: Python 
+    //     2: Rust 
+    //     3: C++
+    //     (write option number)
+    // 1.2. Based on the input Number, update the answers Array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+
+
+    // 2. Call this method whenever the user clicks the "Answer Poll" button; 
+
+    // 3. Create a method 'displayResults' which displays the pool results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string'. display a string like "Pool results are 13, 2, 4, 1".
+
+    // 4. Run the 'displayResults' method at the end of each 'registerNewAnster' method call.
+    // BONUS: Use the 'DisplayResutls' method to display the 2 array in the test data.
+    const poll = {
+        question: 'What is your favourite programming language?',
+        options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
+
+        answers: new Array(4).fill(0),
+        registerNewAnswer: function() {
+            // Get answer
+            const ans = Number(
+                prompt(`${this.question}\n${this.options.join('\n')} \n (Write option number)`)
+            );
+            console.log(ans);
+            
+            // Register answer
+            typeof ans === 'number' && ans < this.options.length && this.answers[ans]++;
+            this.displayResult();
+            this.displayResult('string');
+        },
+        displayResult: function(type = 'array') {
+        //   const type =  prompt('type string or array');
+          if (type === 'array'){
+              console.log(this.answers);
+          }
+          else if (type === "string") {
+              console.log(`Pool results are ${this.answers.join(', ')}`);
+          }
+        }
+    };
+
+    
+    document.body.querySelector('.poll').addEventListener('click',function(){
+       poll.registerNewAnswer();
+        
+    });
+poll.displayResult.call({answers: [5, 2, 3]}, 'string');
+    const newPoll = {
+        answers: [1, 5, 7, 3, 0],
+    }
+    
+     poll.displayResult.call(newPoll, 'string');
+
+
+    console.log(newPoll);
