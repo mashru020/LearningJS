f45d15dc9bb8484a5b512c46baa9a183d56f3589
mashru020 <mashrurrahman16@gmail.com>
2021-09-28 17:36:26 +0600

    L11. Working with Array

--- L11/script.js
+++ L11/script.js
@@ -579,7 +579,7 @@ console.log(movements);
 movements.sort((a, b) => b - a);
 console.log(movements);*/
 
-// 161. More way to creating and filling Array
+/*// 161. More way to creating and filling Array
 const arr = [1, 2, 3, 4, 5, 6];
 console.log(new Array(1, 2, 3, 4, 5, 6));
 
@@ -618,4 +618,70 @@ labelBalance.addEventListener('click', function() {
 
     const movementsUI2 = [...document.querySelectorAll('.movements__value')];
     console.log(movementsUI2);
-});
+});*/
+
+// 163. Coding Challenge #4
+
+const dogs = [
+    {
+        weight:22,
+        curFood: 250,
+        owners: ['Alice', 'Bob']
+    },
+    {
+        weight:8,
+        curFood: 200,
+        owners: ['Matilda']
+    },
+    {
+        weight:13,
+        curFood: 275,
+        owners: ['Sarah', 'John']
+    },
+    {
+        weight:32,
+        curFood: 340,
+        owners: ['Michael']
+    }
+]
+
+// 1.
+dogs.forEach(dog => (dog.recFood = Math.trunc( dog.weight ** 0.75 * 28)
+));
+console.log(dogs);
+
+// 2. 
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+
+console.log(dogSarah);
+console.log(`Sarah's dos is eating too ${dogSarah.curFood > dogSarah.recFood ? 'much' : 'little' }!`);
+
+
+// 3. 
+const ownersEatTooMuch = dogs
+    .filter(dog => dog.curFood > dog.recFood)
+    .flatMap(dog => dog.owners)
+    //.flat();
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs.filter(dog => dog.curFood < dog.recFood)
+    .map(dog => dog.owners).flat();
+    console.log(ownersEatTooLittle);
+
+// 4.
+console.log(`${ownersEatTooMuch.join(' and ') }'s dogs eat too much!'`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+// 6.
+const checkEatingOkay = dog => dog.curFood > (dog.recFood* 0.9) && dog.curFood < (dog.recFood * 1.1); 
+console.log(dogs.some(checkEatingOkay));
+
+// 7.
+console.log(dogs.filter(checkEatingOkay));
+
+// 8.
+const dogsSorted = dogs.slice().sort((a,b) => a.recFood - b.recFood);
+console.log(dogsSorted);
