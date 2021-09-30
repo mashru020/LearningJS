0d30ae660b8bbd53dd6d08026477fd00c4f1f7ee
mashru020 <mashrurrahman16@gmail.com>
2021-09-30 22:24:06 +0600

    172. Operations with date

--- L12/script.js
+++ L12/script.js
@@ -17,14 +17,14 @@ const account1 = {
 		pin: 1111,
 	
 		movementsDates: [
-			'2019-11-18T21:31:17.178Z',
-			'2019-12-23T07:42:02.383Z',
-			'2020-01-28T09:15:04.904Z',
-			'2020-04-01T10:17:24.185Z',
-			'2020-05-08T14:11:59.604Z',
-			'2020-05-27T17:01:17.194Z',
-			'2020-07-11T23:36:17.929Z',
-			'2020-07-12T10:51:36.790Z',
+			'2020-11-18T21:31:17.178Z',
+			'2020-12-23T07:42:02.383Z',
+			'2021-01-28T09:15:04.904Z',
+			'2021-07-01T10:17:24.185Z',
+			'2021-08-08T14:11:59.604Z',
+			'2021-08-27T17:01:17.194Z',
+			'2021-09-24T23:36:17.929Z',
+			'2021-09-29T10:51:36.790Z',
 		],
 		currency: 'EUR',
 		locale: 'pt-PT', // de-DE
@@ -37,14 +37,14 @@ const account1 = {
 		pin: 2222,
 	
 		movementsDates: [
-			'2019-11-01T13:15:33.035Z',
-			'2019-11-30T09:48:16.867Z',
-			'2019-12-25T06:04:23.907Z',
-			'2020-01-25T14:18:46.235Z',
-			'2020-02-05T16:33:06.386Z',
-			'2020-04-10T14:43:26.374Z',
-			'2020-06-25T18:49:59.371Z',
-			'2020-07-26T12:01:20.894Z',
+			'2020-11-01T13:15:33.035Z',
+			'2020-11-30T09:48:16.867Z',
+			'2020-12-25T06:04:23.907Z',
+			'2021-01-25T14:18:46.235Z',
+			'2021-02-05T16:33:06.386Z',
+			'2021-04-10T14:43:26.374Z',
+			'2021-06-25T18:49:59.371Z',
+			'2021-09-26T12:01:20.894Z',
 		],
 		currency: 'USD',
 		locale: 'en-US',
@@ -82,21 +82,35 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions
+const formatMovementDate = function(date){
+	const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
+	const daysPassed = calcDaysPassed(date,new Date())
+	console.log(daysPassed);
+	if(daysPassed === 0)return 'Today';
+	if(daysPassed === 1) return 'Yesterday';
+	if(daysPassed <= 7) return `${daysPassed} days ago`;
+	else {
+		const day = `${date.getDate()}`.padStart(2, 0);
+		const month = `${date.getMonth() + 1}`.padStart(2, 0);
+		const year = date.getFullYear();
+		return `${day}/${month}/${year}`;
+	}	
+}
 const displayMovements = function (acc, sort = false) {
 	containerMovements.innerHTML = '';
 
 	const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
 
 	movs.forEach(function (mov, i) {
+
 		const type = mov > 0 ? 'deposit' : 'withdrawal';
 		const date = new Date(acc.movementsDates[i]);
-		const day = `${date.getDate()}`.padStart(2, 0);
-		const month = `${date.getMonth() + 1}`.padStart(2, 0);
-		const year = date.getFullYear();
+		const displayDate = formatMovementDate(date);
+		
 
-
-		const displayDate = `${day}/${month}/${year}`;
+		
+		
 		const html = `
 			<div class="movements__row">
 				<div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
@@ -450,3 +464,18 @@ const calcDisplaySummary = function (acc) {
 	console.log(future);*/
 
 	// 171. Adding Dates to the Bankist App
+
+	// 172. Operations With Dates
+
+	const future = new Date(2021, 10, 19, 15,23);
+	//console.log(Number(future));
+	console.log(+future);
+
+	const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+	const days1 = calcDaysPassed(
+		new Date(2021, 3, 24), 
+		new Date(2021, 3, 19)
+	);
+	console.log(days1);
+
+
