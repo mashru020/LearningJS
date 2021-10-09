99109e72f030858de3fe7d9f1b55d09c154ba67f
mashru020 <mashrurrahman16@gmail.com>
2021-10-09 14:39:10 +0600

    181. Selecting, Creating and Deleting elements

--- L13/script.js
+++ L13/script.js
@@ -28,3 +28,44 @@ document.addEventListener('keydown', function(e) {
         closeModal();
     }
 })
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+// 181. Selectiong, Creating and Deleting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('#section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements 
+//.insetAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = 'We use cookied for inproved functionality and analytics';
+message.innerHTML = 'We use cookied for inproved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message); //add as a first child 
+header.append(message); // add as a last child
+
+// header.append(message.cloneNode(true)); // if we want two 
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements 
+document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+    //message.remove();
+    message.parentElement.removeChild(message);
+});
+
