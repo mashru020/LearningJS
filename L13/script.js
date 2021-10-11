8527db25b78d36dde9c0c0d8742990ceafcf3ff1
mashru020 <mashrurrahman16@gmail.com>
2021-10-11 06:43:27 +0600

    185, 186 -> Event Propagation Pranctice

--- L13/script.js
+++ L13/script.js
@@ -176,3 +176,31 @@ h1.addEventListener('mouseenter', alertH1 );
 //setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 // way 5 in html
+
+// 185. Event Propagation, Bubblinga and capturing
+// 186. Event Propagation Practice
+// rgb(255,255,255)
+const randomInt = (min, max) =>  Math.floor(Math.random() * ( max - min + 1 ) + min );
+const randomColor = () => `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)},${randomInt(0, 255)})`;
+//console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+    this.style.backgroundColor = randomColor();
+    console.log('LiNK', e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+
+    // Stop propagation
+    //e.stopPropagation();
+});
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+    this.style.backgroundColor = randomColor();
+    console.log('CONTAINER', e.target, e.currentTarget);
+});
+document.querySelector('.nav').addEventListener(
+    'click', 
+    function(e) {
+        this.style.backgroundColor = randomColor();
+        console.log('NAV', e.target, e.currentTarget);
+    },
+    true
+);
