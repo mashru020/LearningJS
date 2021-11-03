a6b87c7cf51703bbc0576db05d310ed860f23839
mashru020 <mashrurrahman16@gmail.com>
2021-11-03 23:50:02 +0600

    214. Coding Challenge #3

--- L14/script.js
+++ L14/script.js
@@ -254,7 +254,7 @@ ford.break();
 ford.speedUS = 50;
 console.log(ford);*/
 
-// 213. Inheritance between Classes : Constructor Functions
+/*// 213. Inheritance between Classes : Constructor Functions
 
 const Person = function (firstName, birthYear) {
     this.firstName = firstName;
@@ -303,4 +303,41 @@ console.log(mike instanceof Object);
 console.dir(Student.prototype.constructor);
 
 Student.prototype.constructor = Student;
-console.dir(Student.prototype.constructor);
+console.dir(Student.prototype.constructor);*/
+
+// 214. Coding Challenge #3
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+    
+}
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+}
+Car.prototype.break = function() {
+    this.speed -= 5;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+    
+}
+const EV = function( make, speed, charge ) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+}
+
+
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.chargeBattery = function(chargeTo){
+    this.charge = chargeTo;
+}
+EV.prototype.accelerate = function() {
+    this.speed += 20 ;
+    this.charge --;
+    console.log(`${this.make} going at at ${this.speed} km/h, with a charge of ${this.charge}%`);
+};
+const tesla =new EV("Tesla", 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.accelerate();
+tesla.break();
