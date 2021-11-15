9c0c7f2d1f5e1ab56c0086f8a0a6f4476a0f6f51
mashru020 <mashrurrahman16@gmail.com>
2021-11-15 22:41:49 +0600

    218. Encapsulation Protected Properties and methods

--- L14/script.js
+++ L14/script.js
@@ -430,37 +430,42 @@ class Account {
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin;
-        this.movements = [];
+        this._pin = pin;
+        this._movements = [];
         this.locale = navigator.language;
 
         console.log(`Thanks for opening an account, ${owner}`);
     }
 
     // Public interface
+    getMovements() {
+        return this._movements;
+    }
+
     deposit(val) {
-        this.movements.push(val);
+        this._movements.push(val);
     }
     withdraw(val) {
         this.deposit(-val);
     }
-    approveLoan(val) {
+    _approveLoan(val) {
         return true;
     }
     requestLoan(val) {
-        if(this.approveLoan(val)) {
+        if(this._approveLoan(val)) {
             this.deposit(val);
             console.log(`Loan approved`);
         }
     }
+
 }
 
 const acc1 = new Account('Mashru', 'EUR', 1111);
 console.log(acc1);
 
 // will work but not a proper way
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 
 acc1.deposit(250);
 acc1.withdraw(140);
@@ -468,4 +473,10 @@ acc1.withdraw(140);
 acc1.requestLoan(1000);
 
 console.log(acc1);
-console.log(acc1.pin);
+//console.log(acc1.pin);
+
+
+
+// 218. Encapsulation Protected Properties and methods
+console.log(acc1.getMovements());
+console.log(acc1._pin);
