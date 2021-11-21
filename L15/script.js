a8d12c10e691f55c174130915e2140156359b6b2
mashru020 <mashrurrahman16@gmail.com>
2021-11-21 21:08:37 +0600

    228. Displaying a map marker

--- L15/script.js
+++ L15/script.js
@@ -19,19 +19,37 @@ if(navigator.geolocation){
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
-    const map = L.map('map').setView(coords, 13);
+    const map = L.map('map').setView(coords, 13); 
+    // console.log(map);
 
+    // 227. Dispaly a map using leaflet library
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: 
         '&copy; <a href="https://www.openstreetmap.org copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+    // L.marker(coords).addTo(map)
+    //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    //     .openPopup();
 
+    // 228. Displaying a map marker
+    map.on('click', function(mapEvent) {
+        //  console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        console.log(lat, lng);
+        L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
+            maxWitdh: 250,
+            minWidth: 50,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+        }))
+        .setPopupContent('Workout')
+        .openPopup();
+    }); 
     }, function(){
         alert('Could not get your position');
+
     })
 };
 // console.log(firstName);
